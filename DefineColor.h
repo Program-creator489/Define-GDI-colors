66b0635fbfea20c5399799d7c4f738818a7f8446
Program-creator489 <kobralu21@gmail.com>
2026-07-22 22:55:02 -0500

    Add files via upload

---
+++ DefineColor.h
@@ -0,0 +1,36 @@
+typedef union _RGBQUAD {
+    COLORREF rgb;
+    struct {
+        BYTE r;
+        BYTE g;
+        BYTE b;
+        BYTE Reserved;
+    };
+} _RGBQUAD, *PRGBQUAD;
+
+int red = 0, green = 0, blue = 0;
+bool ifblue = false;
+
+COLORREF Hue(int length)
+{
+    if (red != length) {
+        red++;
+        if (ifblue)
+            return RGB(red, 0, length);
+        else
+            return RGB(red, 0, 0);
+    }
+    else if (green != length) {
+        green++;
+        return RGB(length, green, 0);
+    }
+    else if (blue != length) {
+        blue++;
+        return RGB(0, length, blue);
+    }
+    else {
+        red = green = blue = 0;
+        ifblue = true;
+        return RGB(0, 0, 0);
+    }
+}
